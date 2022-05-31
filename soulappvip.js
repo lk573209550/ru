8c968768b0d1b211b99c1b22cd852c2a11e3582a
Riika <44261815+lk573209550@users.noreply.github.com>
2022-05-31 18:15:08 +0800

    Create soulappvip.js

---
+++ soulappvip.js
@@ -0,0 +1,16 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+
+var user = {};
+obj.success = true;
+obj.data = {
+    "hasMyMeet": true,
+    "superVIP": true,
+    "showSuperVIP": true,
+    "hasCancelVIPSubscription": true,
+    "leftDay": ntrue,
+    "hasCancelVIPSubOfIAP": true,
+    "needRestoreIAP": true,
+    "hasFlyPackage": false,
+    "remainDay": 9999
+}
