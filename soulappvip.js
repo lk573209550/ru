70abbb1bdeb5196cb3dec2b4cef1f2337bdebc92
Riika <44261815+lk573209550@users.noreply.github.com>
2022-05-31 18:43:56 +0800

    Update soulappvip.js

--- soulappvip.js
+++ soulappvip.js
@@ -1,18 +1,4 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-
-var user = {};
-obj.success = true;
-obj.data = {
-    "hasMyMeet": true,
-    "superVIP": true,
-    "showSuperVIP": true,
-    "hasCancelVIPSubscription": true,
-    "leftDay": ntrue,
-    "hasCancelVIPSubOfIAP": true,
-    "needRestoreIAP": true,
-    "hasFlyPackage": false,
-    "remainDay": 9999
-}
-body = JSON.stringify(obj);
-$done(body);
+let obj = JSON.parse($response.body);
+obj.data["superVIP"] = true;
+obj.data["showSuperVIP"] = true;
+$done({body: JSON.stringify(obj)});
