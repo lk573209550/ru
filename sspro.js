ca8c96ed4ffdc2bbc6f25f056a3dd30048e65742
Riika <44261815+lk573209550@users.noreply.github.com>
2023-09-19 10:28:03 +0800

    Update sspro.js

--- sspro.js
+++ sspro.js
@@ -4,5 +4,6 @@ obj.data["activeMaxScore"] = 80;
 obj.data["totalScore"] = 150;
 obj.data["activeScore"] = 80;
 obj.data["serviceScore"] = 50;
+obj.data["newCourierOrRankTip"] = "超过99.99%的骑手，已没有提升空间";
 $done({body: JSON.stringify(obj)});
 
