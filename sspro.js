5eb4063a8f48a9e9dbc7ca2f690dcb94cefe2d35
Riika <44261815+lk573209550@users.noreply.github.com>
2023-09-19 10:10:09 +0800

    Update sspro.js

--- sspro.js
+++ sspro.js
@@ -1,30 +1,7 @@
-/*
-酷我音乐 解锁会员试听及部分功能
 
-***************************
-QuantumultX:
+let obj = JSON.parse($response.body);
+obj.data["activeMaxScore"] = 100;
+obj.data["totalScore"] = 100;
 
-[rewrite_local]
-^https?://m.ishansong.com/(activationScore/client/home) url script-response-body https://raw.githubusercontent.com/lk573209550/ru/main/sspro.js
-[mitm]
-hostname = m.ishansong.com
+$done({body: JSON.stringify(obj)});
 
-
-**************************/
-
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const Score = '/activeMaxScore/totalScore';
-
-if (url.indexOf(Score) != -1) {
-	obj.data["activeMaxScore"] = "100";
-	obj.data["totalScore"] = "100";
-
-	body = JSON.stringify(obj);
-}
-
-
-
-$done({body});
