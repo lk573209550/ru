d942b18c9fa9ee43a4a3005b6c1741e9262189cb
Riika <44261815+lk573209550@users.noreply.github.com>
2023-09-19 10:25:53 +0800

    Update sspro.js

--- sspro.js
+++ sspro.js
@@ -3,6 +3,6 @@ let obj = JSON.parse($response.body);
 obj.data["activeMaxScore"] = 80;
 obj.data["totalScore"] = 150;
 obj.data["activeScore"] = 80;
-obj.data["ServiceScore"] = 50;
+obj.data["serviceScore"] = 50;
 $done({body: JSON.stringify(obj)});
 
